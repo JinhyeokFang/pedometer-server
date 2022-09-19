840eb15f519c987587773c5cb48c1dd9e9736f55
JinhyeokFang <jinhyeokfang@gmail.com>
2022-09-20 00:00:24 +0900

    add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService: ConfigService = app.get(ConfigService);
+
+
+    const config = new DocumentBuilder()
+        .setTitle('Pedometer Server')
+        .setDescription('The Pedometer Server API Document')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
     await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
